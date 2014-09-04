5d0ff5fea766cb30374400c43c35a4f120a10f68
Tom Nurkkala <tnurkkala@flattop.cse.taylor.edu>
2014-09-04 07:05:12 -0900

    Updates for production use.

--- curriculator/settings.py
+++ curriculator/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'cs2013',
+    'gunicorn',
     'debug_toolbar',
 )
 
@@ -78,10 +79,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
+
+LOGGING = {
+    'version': 1
+}
+
