be1ce597fda4ef7524f4d1d6c5c813341c2d504f
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2015-04-13 20:19:44 -0400

    Checkpoint for co-course reporting

--- cs2013/views.py
+++ cs2013/views.py
@@ -1,9 +1,11 @@
 from __future__ import division
+import textwrap
 
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+import numpy as np
 from rest_framework import viewsets
 
 from .models import *
@@ -102,6 +104,89 @@ def coverage(request):
     knowledge_areas = KnowledgeArea.objects.prefetch_related('knowledge_units').all()
     return render(request, 'cs2013/coverage.html',
                   { 'knowledge_areas': knowledge_areas })
+
+class SequentialMap(object):
+    def __init__(self):
+        self.next_idx = 0
+        self.id_to_idx_map = { }
+        self.idx_to_obj_map = { }
+
+    def id_to_idx(self, obj):
+        idx = None
+        if obj.id in self.id_to_idx_map:
+            idx = self.id_to_idx_map[obj.id]
+        else:
+            idx = self.id_to_idx_map[obj.id] = self.next_idx
+            self.next_idx += 1
+            self.idx_to_obj_map[idx] = obj
+        return idx
+
+    def idx_to_obj(self, idx):
+        return self.idx_to_obj_map[idx]
+
+    @property
+    def count(self):
+        return self.next_idx
+
+@login_required
+def co_outcomes(request):
+    seq_course_map = SequentialMap()
+    seq_outcome_map = SequentialMap()
+
+    num_courses = Course.objects.count()
+    num_outcomes = LearningOutcome.objects.count()
+    outcome_matrix = np.zeros((num_outcomes, num_courses), dtype=np.uint16)
+
+    course_count = outcome_count = 0
+    for course in Course.objects.all():
+        course_count += 1
+        for outcome in course.learning_outcomes.all():
+            outcome_count += 1
+            course_idx = seq_course_map.id_to_idx(course)
+            outcome_idx = seq_outcome_map.id_to_idx(outcome)
+            outcome_matrix[outcome_idx, course_idx] = 1
+
+    # Do the "RTR" calculation
+    co_outcomes = np.dot(outcome_matrix.transpose(), outcome_matrix)
+
+    non_empty_courses = seq_course_map.count
+    non_empty_outcomes = seq_outcome_map.count
+
+    context = { 'courses': [ ] }
+
+    for course_idx in range(0, non_empty_courses):
+        course = seq_course_map.idx_to_obj(course_idx)
+        outcome_row = outcome_matrix[:, course_idx]
+        outcome_indexes = np.nonzero(outcome_row)[0]
+
+        co_course_data = [ ]
+        for co_course_idx in range(0, non_empty_courses):
+            if course_idx == co_course_idx:
+                continue            # Skip relationship with self
+            weight = co_outcomes[course_idx, co_course_idx]
+            if weight > 0:
+                co_course_data.append({ 'weight': weight,
+                                        'co_course_idx': co_course_idx })
+
+        context_co_course_data = [ ]
+        for co_course_datum in sorted(co_course_data, key=lambda x: x['weight'], reverse=True):
+            weight = co_course_datum['weight']
+            co_course_idx = co_course_datum['co_course_idx']
+
+            co_outcome_row = outcome_matrix[:, co_course_idx]
+            co_outcome_indexes = np.nonzero(co_outcome_row)[0]
+            common_outcome_indexes = np.intersect1d(outcome_indexes, co_outcome_indexes)
+
+            outcomes = [ seq_outcome_map.idx_to_obj(idx) for idx in common_outcome_indexes ]
+
+            context_co_course_data.append({ 'course': seq_course_map.idx_to_obj(co_course_idx),
+                                            'weight': weight,
+                                            'outcomes': outcomes })
+
+        context['courses'].append(context_co_course_data)
+
+    return render(request, 'cs2013/co_outcomes.html', context)
+
 
 #### API ################################################################
 
